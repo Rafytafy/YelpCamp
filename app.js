2c5170f6b750aceae140555d067bc7ad7ca0aa63
Rafael <rafaelmonte1997@gmail.com>
2020-11-27 17:59:06 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const commentRoutes 	= require("./routes/comments"),
 	  campgroundRoutes 	= require("./routes/campgrounds"),
 	  indexRoutes 		= require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://Rafael:Hotdog123@mern-stack.acx9h.mongodb.net/yelp_camp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
